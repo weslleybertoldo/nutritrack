5c3719a8d7704fee3f1601b0ca4a3efdd7815ed0
Weslley Bertoldo <55102421+weslleybertoldo@users.noreply.github.com>
2026-03-20 14:53:43 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")!).render(<App />);
